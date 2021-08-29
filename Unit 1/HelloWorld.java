b64d93a6c02638b7951099663b887ec6b0b156d9
Joel Hammer <jhammer@friendsbalt.org>
2021-08-29 19:52:33 -0400

    created the framework for a Hello, World program

---
+++ Unit 1/HelloWorld.java
@@ -0,0 +1,24 @@
+// All java programs are made up of classes. A class can be
+// either public or private (or some other options...), but
+// for now, all of your programs should begin with the
+// declaration "public class" followed by the name of the
+// class. Note that the class name must be the same as the
+// name of the java file!
+public class HelloWorld {
+	
+	// A Java program is separated into methods, which are
+	// blocks of code that accomplish an individual task.
+	// In order to run a java program it must have a method
+	// that begins with "public static void main(String[] args).
+	// The code that the method runs is enclosed between curly
+	// braces as below. We'll cover what all these words mean
+	// throughout the course.
+	public static void main(String[] args) {
+		
+		// This bit of code tells the computer to type something
+		// to the command line.
+		System.out.println("Goodbye, Mars!");
+		
+	} //Curly braces end the body of a method.
+	
+} // Just like methods, the body of a class ends with a curly brace.
